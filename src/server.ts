245c3f21d879f83d89147961cafc43150e06e2ee
stagfoo <alexanderddking@@gmail.com>
2021-12-29 09:30:42 +0900

    ✨ deploy fixes

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
-var port = 9999;
+var port = process.env.PORT;
 
 server.listen(port, function () {
   console.log('Server listening at port %d', port);
